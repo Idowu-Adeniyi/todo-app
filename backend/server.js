42aba3c0ce26372d73b9307ed5d915c598b00d4c
Idowu Adeniyi <adeniyi.idowu500@gmail.com>
2026-05-30 20:23:03 -0400

    Initialize Node project with package.json inside backend

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+// const express = require("express");
+// const app =  app.express();
+
+
+// const PORT = process.env.PORT|| 3000
+
+//app.use(express.json());
+
+// app.get("/", (req, res)=>{
+//     res.send("ok")
+// })
+
+// app.listen(port, ()=>{
+//     console.log(`server is running on localhost:${port}`)
+// })
+
+
