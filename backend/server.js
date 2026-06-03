95b55e03c42dd28151ab2336efbb552ae81db067
Idowu Adeniyi <adeniyi.idowu500@gmail.com>
2026-06-03 00:06:48 -0400

    Implemented GET all, GET by ID, and POST routes for todos API

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,57 @@
 import express from "express";
-import bodyParser from "body-parser";
-import path, {dirname} from "path"
-import { fileURLToPath } from "url";
+// import bodyParser from "body-parser";
+// import {join, dirname} from "path"
+// import { fileURLToPath } from "url";
 
 //set your file path
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = process.env.PORT || 3000
 
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended:true}))
+// app.use(bodyParser.urlencoded({extended:true}));
 
- 
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../", "frontend", "index.html"));
+
+let todos = [
+    {id:1, task: "Learn Express JSON APIs", completed: false },
+    {id:2, task: "Test CRUD routes in Postman", completed: false }
+];
+
+
+// GET ALL TODOS
+app.get("/api/todos", (req, res) => {
+    //   res.sendFile(join(__dirname, "..",  "/frontend/index.html"));
+    res.json(todos);
+
+
+    // GET A SINGLE TODO BY ID
+app.get("/api/todos/:id", (req, res) => {
+    const todoId = parseInt(req.params.id); 
+
+    //Search your database array for that specific item
+    const foundTodo = todos.find(todo => todo.id === todoId);
+
+    //Send it if found , or give 404 error message if doesnt exist
+    if(foundTodo){
+        res.json(foundTodo);
+    }else {
+        res.status(404).json({error: "Todo item not found"});
+    }
+});
+
+
 })
-
-app.post("/submit", (req, res) => {
-    res.send(req.body);
-    console.log(req.body);
+//CREAT A TODO
+app.post("/api/todos", (req, res) => {
+    const newTodo = {
+        id: todos.length + 1,
+        task: req.body.task,   // Pulls from JSON request body
+        completed: false
+    };
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
 })
 
 
